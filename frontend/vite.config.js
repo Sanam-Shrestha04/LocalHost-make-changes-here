3b7377213d439f7d7e82e68380a0de5586675dc3
Swechhya <swechhhya.stha@gmail.com>
2025-11-01 00:34:23 +0545

    F-VITEE

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,12 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-   
   plugins: [react(), tailwindcss()],
   server: {
     host: true, // allows Vite to listen on any host
     port: 5173, // optional, your dev port
   },
   preview: {
-    allowedHosts: ['localhost-old-osbs.onrender.com'], // add your Render host here
+    allowedHosts: ['taskforgem.onrender.com'], // add your Render host here
   },
 });
